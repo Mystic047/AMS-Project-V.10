b1bc9f74a9f8d0341f0b1edff1375f73eb34233e
Kingsomsri45 <640112418053@bru.ac.th>
2024-06-12 23:24:03 +0700

    fix : แก้ Form create,edit

--- resources/views/admin/editView/activityEdit.blade.php
+++ resources/views/admin/editView/activityEdit.blade.php
@@ -27,15 +27,11 @@
                         <label for="inputAddress" class="form-label">ID</label>
                         <input type="text" name='students_id'class="form-control" id="inputAddress" placeholder="ถ้า Auto ก็เอาออก">
                     </div>  --}}
-                    <div class="col-md-2">
+                    <div class="col-md-6">
                         <label for="email" class="form-label">รหัสกิจกรรม</label>
                         <input type="email" name='email' class="form-control" id="inputEmail4">
                     </div>
-                    <div class="col-md-5">
-                        <label for="password" class="form-label">ชื่อกิจกรรม</label>
-                        <input type="password" name='password' class="form-control" id="password">
-                    </div>
-                    <div class="col-md-5">
+                    <div class="col-md-6">
                         <label for="password" class="form-label">ชื่อกิจกรรม</label>
                         <input type="password" name='password' class="form-control" id="password">
                     </div>
