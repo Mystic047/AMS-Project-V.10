85396a620ccc5f3c959a1bb8ce5cd1bd8018854b
Kingsomsri45 <640112418053@bru.ac.th>
2024-06-23 17:42:48 +0700

    feat : ทำ Style หน้า login กับ Sweet alert เวลารหัสผิด ทั้ง2 หน้า login

--- resources/views/adminlogin.blade.php
+++ resources/views/adminlogin.blade.php
@@ -8,16 +8,16 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://unpkg.com/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.css">
+    <link rel="stylesheet"  href="{{ asset('css/adminlogin.css') }}">>
 </head>
 
-<body class="bg-light">
-    <!-- Login 8 - Bootstrap Brain Component -->
-    <!-- Login 1 - Bootstrap Brain Component -->
-    <div class="bg-light py-3 py-md-5 d-flex align-items-center justify-content-center vh-100">
+<body>
+    <div class="py-3 py-md-5 d-flex align-items-center justify-content-center vh-100">
         <div class="container">
             <div class="row justify-content-md-center">
                 <div class="col-12 col-md-11 col-lg-8 col-xl-7 col-xxl-6">
-                    <div class="bg-white p-4 p-md-5 rounded shadow-sm">
+                    <div class="form-container">
                         <div class="row">
                             <div class="col-12">
                                 <div class="text-center mb-5">
@@ -29,19 +29,15 @@
                             @csrf
                             <div class="row gy-3 gy-md-4 overflow-hidden">
                                 <div class="col-12">
-                                    <label for="email" class="form-label">Email <span
-                                            class="text-danger">*</span></label>
+                                    <label for="email" class="form-label">Email <span class="text-danger">*</span></label>
                                     <div class="input-group">
-                                        <input type="email" class="form-control" name="email" id="email"
-                                            required>
+                                        <input type="email" class="form-control" name="email" id="email" required>
                                     </div>
                                 </div>
                                 <div class="col-12">
-                                    <label for="password" class="form-label">Password <span
-                                            class="text-danger">*</span></label>
+                                    <label for="password" class="form-label">Password <span class="text-danger">*</span></label>
                                     <div class="input-group">
-                                        <input type="password" class="form-control" name="password" id="password"
-                                            value="" required>
+                                        <input type="password" class="form-control" name="password" id="password" value="" required>
                                     </div>
                                 </div>
                                 <div class="col-12">
@@ -50,27 +46,34 @@
                                     </div>
                                 </div>
                             </div>
-                            @if (session('success'))
-                                <div class="alert alert-success">
-                                    <li>{{ session('success') }}</li>
-                                </div>
-                            @endif
-                            @if ($errors->any())
-                                <div class="alert alert-danger">
-                                    @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </div>
-                            @endif
                         </form>
-
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            @if (session('success'))
+                swal({
+                    title: "Success",
+                    text: "{{ session('success') }}",
+                    icon: "success",
+                    button: "OK",
+                });
+            @endif
+            @if ($errors->any())
+                swal({
+                    title: "รหัสผ่านไม่ถูกต้อง",
+                    text: "{{ $errors->first() }}",
+                    icon: "error",
+                    button: "ตกลง",
+                });
+            @endif
+        });
     </script>
 </body>
 
