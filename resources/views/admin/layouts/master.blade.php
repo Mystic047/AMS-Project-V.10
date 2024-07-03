caa985539c957f1ed2917ed7610f16408ce4b83f
MYSE4 <640112418047@bru.ac.th>
2024-07-04 05:54:21 +0700

    feat : comment unused script

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -11,8 +11,8 @@
 
   <!-- CSS Libraries -->
  
-  <link rel="stylesheet" href="{{ asset('backend/assets/modules/weather-icon/css/weather-icons.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('backend/assets/modules/weather-icon/css/weather-icons-wind.min.css') }}">
+  {{-- <link rel="stylesheet" href="{{ asset('backend/assets/modules/weather-icon/css/weather-icons.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('backend/assets/modules/weather-icon/css/weather-icons-wind.min.css') }}"> --}}
 
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
@@ -82,22 +82,20 @@
   <!-- General JS Scripts -->
   <script src="{{ asset('backend/assets/modules/jquery.min.js') }}"></script>
   <script src="{{ asset('backend/assets/modules/popper.js') }}"></script>
-  <script src="{{ asset('backend/assets/modules/tooltip.js') }}"></script>
+  {{-- <script src="{{ asset('backend/assets/modules/tooltip.js') }}"></script> --}}
   <script src="{{ asset('backend/assets/modules/bootstrap/js/bootstrap.min.js') }}"></script>
   <script src="{{ asset('backend/assets/modules/nicescroll/jquery.nicescroll.min.js') }}"></script>
-  <script src="{{ asset('backend/assets/modules/moment.min.js') }}"></script>
+  {{-- <script src="{{ asset('backend/assets/modules/moment.min.js') }}"></script> --}}
 
   <!-- JS Libraies -->
-  <script src="{{ asset('backend/assets/modules/simple-weather/jquery.simpleWeather.min.js') }}"></script>
-  <script src="{{ asset('backend/assets/modules/chart.min.js') }}"></script>
-  <script src="{{ asset('backend/assets/modules/jqvmap/dist/jquery.vmap.min.js') }}"></script>
-  <script src="{{ asset('backend/assets/modules/jqvmap/dist/maps/jquery.vmap.world.js') }}"></script>
-  <script src="{{ asset('backend/assets/modules/summernote/summernote-bs4.js') }}"></script>
-  <script src="{{ asset('backend/assets/modules/chocolat/dist/js/jquery.chocolat.min.js') }}"></script>
+  {{-- <script src="{{ asset('backend/assets/modules/simple-weather/jquery.simpleWeather.min.js') }}"></script> --}}
+  {{-- <script src="{{ asset('backend/assets/modules/chart.min.js') }}"></script> --}}
+  {{-- <script src="{{ asset('backend/assets/modules/jqvmap/dist/jquery.vmap.min.js') }}"></script> --}}
+  {{-- <script src="{{ asset('backend/assets/modules/jqvmap/dist/maps/jquery.vmap.world.js') }}"></script> --}}
+  {{-- <script src="{{ asset('backend/assets/modules/summernote/summernote-bs4.js') }}"></script> --}}
+  {{-- <script src="{{ asset('backend/assets/modules/chocolat/dist/js/jquery.chocolat.min.js') }}"></script> --}}
   <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 
-  <!-- Page Specific JS  -->
-  <script src="{{ asset('backend/assets/js/page/index-0.js') }}"></script>
 
   <!-- Template JS File -->
   <script src="{{ asset('backend/assets/js/scripts.js') }}"></script>
