3eae8355af7c91f996dbd1c9194c27bbd54a94f3
Kingsomsri45 <640112418053@bru.ac.th>
2024-08-23 20:21:54 +0700

    feat : ทำ certificate ให้ตรงแบบ

--- resources/views/pdf/certificate.blade.php
+++ resources/views/pdf/certificate.blade.php
@@ -1,5 +1,32 @@
+<?php
+
+function thaiDate($date) {
+    $months = [
+        "01" => "มกราคม",
+        "02" => "กุมภาพันธ์",
+        "03" => "มีนาคม",
+        "04" => "เมษายน",
+        "05" => "พฤษภาคม",
+        "06" => "มิถุนายน",
+        "07" => "กรกฎาคม",
+        "08" => "สิงหาคม",
+        "09" => "กันยายน",
+        "10" => "ตุลาคม",
+        "11" => "พฤศจิกายน",
+        "12" => "ธันวาคม"
+    ];
+
+    $year = date("Y", strtotime($date)) + 543; // แปลงปีคริสต์ศักราชเป็นพุทธศักราช
+    $month = $months[date("m", strtotime($date))]; // แปลงเดือนเป็นภาษาไทย
+    $day = date("d", strtotime($date)); // วันในเดือน
+
+    return $day . " " . $month . " " . $year;
+}
+
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="th">
 <head>
     <meta charset="UTF-8">
     <style>
@@ -59,12 +86,14 @@
 </head>
 <body>
     <div class="certificate-content">
-        <img src="{{ public_path('storage/certificate/certificate.png') }}" alt="Certificate Image" class="certificate-image">
+        <img src="{{ public_path('storage/certificate/Green Gold Elegant Certificate .png') }}" alt="Certificate Image" class="certificate-image">
         <div class="text-content">
+            <br>
             <h2>{{ $data->student->firstName }} {{ $data->student->lastName }}</h2>
+            <br>
             <h2>{{ $data->activity->actName }}</h2>
-            <h2>{{ $data->activity->actDate }}</h2>
-            <h2>{{ $data->activity->actHour }}</h2>
+            <h2><?php echo thaiDate($data->activity->actDate); ?></h2>
+            <h2>รวมระยะเวลา {{ $data->activity->actHour }}  ชั่วโมง</h2>
         </div>
     </div>
 </body>
